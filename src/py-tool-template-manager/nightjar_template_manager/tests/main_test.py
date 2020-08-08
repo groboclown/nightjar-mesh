4a9ac64e7f5acdb1fb35fd552208309b64542e99
Groboclown <matt@groboclown.net>
2020-08-07 20:31:08 -0500

    Add some debugging to find out why travisci is failing.

--- src/py-tool-template-manager/nightjar_template_manager/tests/main_test.py
+++ src/py-tool-template-manager/nightjar_template_manager/tests/main_test.py
@@ -71,6 +71,7 @@ class MainTest(unittest.TestCase):
             main.main(['main.py', '-C', config_file, '--document-type', 'discovery-map', 'pull'])
             self.fail('Did not raise an error')  # pragma no cover
         except ExtensionPointRuntimeError as err:
+            print("Raised " + repr(err))
             self.assertIsNotNone(
                 re.compile(
                     r"ExtensionPointRuntimeError\('data_store: Failed running fetch "
